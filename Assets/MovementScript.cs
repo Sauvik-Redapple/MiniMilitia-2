d771f6351a54f39a7da242c05b523eb9574e8941
SunhikPramanik <46312401+SunhikPramanik@users.noreply.github.com>
2022-07-05 16:06:46 +0530

    update

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    private Vector3 p_position;
+    private float p_power = 40f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        p_position = this.transform.position + new Vector3(10f, 10f, 10f);
+        if (p_power > 0)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                //Debug.Log("Going up");
+                if (this.transform.position == p_position)
+                {
+                    this.transform.position = p_position;
+                }
+                p_power = p_power - 0.5f;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Debug.Log("Jumping up");
+                this.GetComponent<Rigidbody>().AddForce(Vector3.up * 8f, ForceMode.Impulse);
+                p_power = p_power - 0.3f;
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                //Debug.Log("Going front");
+                this.GetComponent<Rigidbody>().AddForce(Vector3.forward * 1f, ForceMode.Impulse);
+            }
+        }
+
+        if (p_power < 40f)
+        {
+            p_power = p_power + 0.1f;
+        }
+        Debug.Log(p_power);
+    }
+}
--- Assets/militia-warfare-master/militia-warfare-master/js/Constants.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Asim on 4/15/2017.
- */
-var TILE_SIZE = 32;
